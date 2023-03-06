ad50d46a7d9125a8bfa2a14251a5fe7fec8738e1
ijeoma <oijeoma11@gmail.com>
2023-03-06 13:56:37 +0100

    user session added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const config = require('./config/config')
-const mongodbConnect = require('./db/mongodb')
+const config = require('./config/config') //configuration folder
+const session = require('express-session');
+const device = require('express-device');
+const mongodbConnect = require('./db/mongodb') //database connection
+// const sessionMiddleware = require('./session/session') //session middleware
 
 const app = express()
 
@@ -9,16 +12,53 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// session middleware
+// app.use(sessionMiddleware)
+
 // mongodb connection
 mongodbConnect
 
+// USER SESSION
+// Use express-device to detect the device type
+app.use(device.capture());
+
+// Use express-session to store the user session
+app.use(                
+  session({
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+// Middleware to store user session based on device type
+app.use((req, res, next) => {     
+  const deviceId = req.device.type.toUpperCase();
+  req.session[deviceId] = req.session[deviceId] || {};
+  next();
+});
 
 
 
-// home route
+
+// Route to test session storage
 app.get('/', (req, res) => {
-    res.send('restaurant chatbox running...');
-})
+    const deviceId = req.device.type.toUpperCase();
+    req.session[deviceId] = req.session[deviceId] || {};
+    req.session[deviceId].count = req.session[deviceId].count || 0;
+    req.session[deviceId].count += 1;
+    res.send(
+      `Device type: ${req.device.type}, Count: ${req.session[deviceId].count}`
+    );
+    console.log(req.session);
+  });
+
+
+// // home route
+// app.get('/', (req, res) => {
+//     res.send('restaurant chatbox running...');
+// })
 
 
 
--- config/config.js
+++ config/config.js
@@ -2,5 +2,6 @@ require('dotenv').config()
 
 module.exports = {
     PORT: process.env.PORT, 
-    MONGODB_URL: process.env.MONGODB_URL
+    MONGODB_URL: process.env.MONGODB_URL,
+    SESSION_SECRET: process.env.SESSION_SECRET
 }
